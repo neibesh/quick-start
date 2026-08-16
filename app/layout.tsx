ab5ea045d7e43e906ed4e7d027462d92544b6600
Chris Neighbour <chris.neighbour@me.com>
2026-08-16 22:21:19 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,27 +3,28 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Get started",
-  description: "Jump to Google, VolunteerHub, eAcademy, or scan a QR code to open a website.",
+	title: "Geraldton VFRS :: Get Started",
+	description:
+		"Jump to Google, VolunteerHub, eAcademy, or scan a QR code to open a website.",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+		>
+			<body className="min-h-full flex flex-col">{children}</body>
+		</html>
+	);
 }
